c51ef24eb448e4daadb7547ecbfd73188196bb96
mateo1665 <89844584+mateo1665@users.noreply.github.com>
2021-10-26 11:27:03 +0200

    Add files via upload

---
+++ tafelmanieren.py
@@ -0,0 +1,14 @@
+for i in range(1,11):
+    print(i*4)
+
+for x in range(1,30,1):
+    print(x)
+
+for AM in range(1,13):
+    print(AM)
+
+for PM in range(1,13):
+    print(PM)
+
+for evengetal in range(20,52,2):
+    print(evengetal)
